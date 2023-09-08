8babd396f2c29637ee9ddf0e4230085290297b76
amritanshu_dash <amritanshudash6677@gmail.com>
2023-09-08 17:29:40 +0530

    Initial Commit

---
+++ Sentimenti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sentimenti
+//
+//  Created by Amritanshu Dash on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
